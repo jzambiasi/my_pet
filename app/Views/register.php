6446817d1c6b468f707f8061936470b4d8863e3f
jzambiasi <jefersonzambiasi@gmail.com>
2023-10-06 00:03:30 -0300

    novas

--- app/Views/register.php
+++ app/Views/register.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <link rel="stylesheet" href="<?= base_url('../../../assets/css/style.css'); ?>">
     <title>Registro de Usuário</title>
 </head>
 <body>
@@ -15,6 +16,7 @@
         </div>
     <?php endif; ?>
 
+    <div class="register">
     <form method="POST" action="/register">
         <label for="email">Email:</label>
         <input type="email" id="email" name="email" required><br>
@@ -23,8 +25,12 @@
         <input type="password" id="password" name="password" required><br>
         <input type="submit" value="Registrar">
     </form>
-    <video width="640" height="360" controls>
-    <source src="../../../assets/video/MY_PET_1.mp4" type="video/mp4">
-</video>
+</div>
+
+<div class="video-container">
+    <video class="video" width="640" height="360" controls>
+        <source src="../../../assets/video/MY_PET_1.mp4" type="video/mp4">
+    </video>
+</div>
 </body>
 </html>
